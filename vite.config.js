6bb17765c1c896efa84d6346653a1ec227b857d3
puneetp8130-sys <puneetp8130@gmail.com>
2026-09-02 22:20:32 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/rosenlilly/",
+
   plugins: [
+    react(),
     tailwindcss(),
   ],
-})
+});
